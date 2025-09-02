06d7944ed61e5d89c108682b6abe43b58f9efeab
Matthew Manning <mattlovestech@github.com>
2025-09-02 15:10:33 -0400

    test

--- class1.sql
+++ class1.sql
@@ -1,2 +1,10 @@
 .mode column
 select * from facilities; -- select all rows and columns from the facilities table
+
+/* here is how you write
+multiline comments in SQL */
+
+/* select specific columns from a table */
+select name, member_cost from facilities; 
+
+select first_name, last_name from members;
