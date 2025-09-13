4321e8bfb085b5f9236639bb62d6f51e7bb189ad
mattlovestech <matthewmanning2014@gmail.com>
2025-09-13 15:43:12 -0400

    finished up work for module 3

---
+++ Desktop/databases/assignment3.sql
@@ -0,0 +1,26 @@
+/* Intro to Databases for Business Analytics - Assignment #3
+Completed by: */
+
+/* 1.   Write a query that returns that average GMAT score for all sponsored students 
+        (sponsored_student = 1), rounded to the nearest decimal point */
+       
+/* 2.   Write a query that returns the average GMAT score by prior_industry, 
+        rounded to the nearest decimal */
+
+/* 3.   Write a query that returns the highest overall compensation package, 
+        including salary, bonus, and equity */
+
+/* 4.   Write a query to determine how many students born in 1990 worked in each 
+        industry before school */
+
+/* 5.   Write a query that returns that total number of offers received by the
+        student body in each month. */
+
+/* 6.   Return a table of the number of offers made by each company showing only 
+        companies that have made more than 40 offers */
+
+/* 7.   Write a query to return the first name and last name as a SINGLE COLUMN and 
+        GMAT score of the first five student who received an offer from Google */
+
+/* 8.   Write a query to return the company names for the 10 companies who had the 
+        most ACCEPTED job offers, ordered from most to least */
---
+++ Desktop/dbs/class3.sql
@@ -0,0 +1,103 @@
+/* aggregate functions continue */
+
+select count(*) from members;
+
+/*
+avg()
+min()
+max()
+sum()
+count()
+round( can have function inside of it)
+*/
+
+select count(distinct address) from members; --we can either look for *, column, or distinct column
+/* 3.2 Challenge: Aggregate Functions Continued */
+/* avg member cost for faciliaties */
+select avg(member_cost) as avg_member_cost from facilities;
+/* avg guest cost for faciliaties */
+select avg(guest_cost) as avg_guest_cost from facilities;
+/* total initial outlay for faciliaties */
+select sum(initial_outlay) as total_initial_outlay from facilities;
+
+/* members who made at least one booking */
+
+select count( distinct member_id) from bookings;
+
+select count(*) 
+from (select distinct member_id from bookings);
+
+/*aggregate functions with where clauses*/
+
+select count(*) 
+from facilities
+ where guest_cost > 10;
+
+/*3.6 Challenge: Where Plus Aggregate Functions*/
+ /*how many facilites have a member cost*/
+select count(*) from facilities where member_cost > 0;
+ /*avg monthly maintence for facilites < 3000 */
+select avg(monthly_maintenance) 
+from facilities where monthly_maintenance < 3000;
+
+ /* whcih facilits has the most expensive guest cost under $80 */
+ select * from facilities 
+ where guest_cost < 80 
+ order by guest_cost desc limit 1;
+
+ /*aggregate functions by group*/
+
+select recommended_by, count(*) 
+from members where recommended_by is not null
+group by recommended_by
+order by count(*) desc
+;
+
+/*3.9 Challenge: Aggregate Functions By Group*/
+/*table of with member last name, frequency of last name descending*/
+select last_name, count(*) as frequency
+from members 
+group by last_name 
+order by frequency desc;
+
+/* no of slots booked by facility*/
+select facility_id, sum(slots) as total_slots
+from bookings
+group by facility_id
+order by total_slots desc;
+
+/*3.12 Challenge: Aggregate Functions By Group 2*/
+/*  # of slots booked per facility for sept 2012*/
+select facility_id, sum(slots) as total_slots
+from bookings
+where start_time between '2012-09-01' and '2012-10-01'
+group by facility_id
+order by total_slots desc;
+
+/*having limits down the number of groups like where does*/
+select facility_id, sum(slots) as total_slots
+from bookings
+group by facility_id
+having total_slots > 1000 --in postgress we have to just say sum(slots) because aliases are not allowed in having
+order by total_slots desc;
+
+/* grouping things by date*/
+select start_time, 
+strftime('%Y-%m', start_time) as month
+from bookings
+limit 10;
+
+/* grouping things by month*/
+select start_time, 
+strftime('%Y-%m', start_time) as month,
+sum(slots) as total_slots
+from bookings
+group by month
+;
+
+/*group by facility and by month*/
+select facility_id,
+strftime('%Y-%m', start_time) as month,
+sum(slots) as total_slots
+from bookings
+group by facility_id, month;
