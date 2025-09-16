90d78d3297ba6bd2f40137fcdfd51c703e7ffc7e
Matthew Manning <mattlovestech@github.com>
2025-09-16 15:03:56 -0400

    get started

--- Desktop/attendance/attendance.sql
+++ Desktop/attendance/attendance.sql
@@ -6,7 +6,9 @@ run the below in terminal
 ----
 psql postgres://student:password@introtodatabases.cqbjxttyshvk.us-east-1.rds.amazonaws.com:5432/introtodatabases
 
-
+then run \include attendance.sql
 */
 
-insert into attendance (first_name, last_name, uni) values ('Matthew', 'Manning', 'mlm2387');
+-- insert into attendance (first_name, last_name, uni) values ('Matthew', 'Manning', 'mlm2387');
+ select * from attendance where uni = 'mlm2387';
+
