3d0cc52d90f8985e787a18c9b93fe06af8b069dd
Matthew Manning <mattlovestech@github.com>
2025-09-23 14:25:49 -0400

    added the analysis portion

--- Desktop/attendance/attendance.sql
+++ Desktop/attendance/attendance.sql
@@ -8,24 +8,24 @@ psql postgres://student:password@introtodatabases.cqbjxttyshvk.us-east-1.rds.ama
 
 then run \include attendance.sql
 */
-
--- insert into attendance (first_name, last_name, uni) values ('Matthew', 'Manning', 'mlm2387');
  select * from attendance where uni = 'mlm2387';
- select date_trunc('day', timestamp) as day, 
- count(distinct timestamp) as unique_sign_ins
- from attendance
- group by day;
+insert into attendance (first_name, last_name, uni) values ('Matthew', 'Manning', 'mlm2387');
+ select * from attendance where uni = 'mlm2387';
+--  select date_trunc('day', timestamp) as day, 
+--  count(distinct timestamp) as unique_sign_ins
+--  from attendance
+--  group by day;
 
- select date_trunc('day', timestamp) as day, 
- count(distinct uni) as unique_uni_sign_ins
- from attendance
- group by day;
+--  select date_trunc('day', timestamp) as day, 
+--  count(distinct uni) as unique_uni_sign_ins
+--  from attendance
+--  group by day;
 
  /*sign ins per uni*/
- select uni, count(*) as sign_ins
- from attendance
- group by uni
- order by sign_ins desc;
+--  select uni, count(*) as sign_ins
+--  from attendance
+--  group by uni
+--  order by sign_ins desc;
  
  /* UNIQUE SIGINS PER DAY */
 
